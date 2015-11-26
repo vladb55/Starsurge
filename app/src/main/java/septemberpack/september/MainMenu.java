bb164adab3b92ff618733a84953197415470e3b9
vlad <plzdiebyethx@gmail.com>
2015-11-27 01:58:27 +0600

    Запретить поворот экрана
    Запрещен поворот экрана

--- app/src/main/java/septemberpack/september/MainMenu.java
+++ app/src/main/java/septemberpack/september/MainMenu.java
@@ -2,13 +2,8 @@ package septemberpack.september;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 
 // Класс главного меню, содержит кнопку старта и ее слушателя
@@ -33,27 +28,5 @@ public class MainMenu extends Activity {
                 GamePanel.score = 0;
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
